78042f421c23a2f2e5f7ecb7e8fc7081e826ebef
leesuyong849 <toosee1005@cau.ac.kr>
2025-02-03 22:04:51 +0900

    FEAT: 임시 완료

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,11 @@
 package christmas;
 
+import christmas.service.Program;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Program program = new Program();
+        program.play();
     }
 }
